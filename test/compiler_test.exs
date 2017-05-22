a196482211585def02b23c601a3c3010a0b9d261
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-05-22 22:26:42 +0700

    finish compiler tests

--- test/compiler_test.exs
+++ test/compiler_test.exs
@@ -3,100 +3,82 @@ defmodule CompilerTest do
 
   alias Slime.Compiler
 
+  alias Slime.Parser.Nodes.HTMLNode
+  alias Slime.Parser.Nodes.EExNode
+  alias Slime.Parser.Nodes.VerbatimTextNode
   alias Slime.Parser.Nodes.DoctypeNode
 
-  test "doctype" do
+  test "renders doctype" do
     tree = [%DoctypeNode{name: "html"}]
     assert Compiler.compile(tree) == "<!DOCTYPE html>"
   end
 
-  #
-  # test "renders simple nesting" do
-  #   tree = [%HTMLNode{tag: :div,
-  #       attributes: [id: {:eex, content: "variable"}, class: ["class"]],
-  #       children: [%HTMLNode{tag: :p,
-  #           children: [%TextNode{content: "Hello World"}]}]}]
-  #
-  #   expected = """
-  #   <div
-  #   <% slim__k = "id"; slim__v = variable %>
-  #   <%= if slim__v do %>
-  #    <%= slim__k %>
-  #   <%= unless slim__v == true do %>
-  #   ="<%= slim__v %>"<% end %><% end %> class="class">
-  #   <p>Hello World</p>
-  #   </div>
-  #   """ |> String.replace("\n", "")
-  #   assert Compiler.compile(tree) == expected
-  # end
-  #
-  # test "renders eex code with strings containing 'do'" do
-  #   tree = [%EExNode{
-  #     attributes: [inline: true],
-  #     content: ~s(number_input f, :amount, class: "js-donation-amount")
-  #   }]
-  #
-  #   expected = ~s(<%= number_input f, :amount, class: "js-donation-amount" %>)
-  #   assert Compiler.compile(tree) == expected
-  # end
-  #
-  # test "renders eex code with inline do: block" do
-  #   tree = [%EExNode{
-  #     attributes: [inline: true],
-  #     content: ~s(if true, do: "ok")
-  #   }]
-  #
-  #   expected = ~s(<%= if true, do: "ok" %>)
-  #   assert Compiler.compile(tree) == expected
-  # end
-  #
-  # test "renders eex code with one-line functions" do
-  #   tree = [%EExNode{
-  #     attributes: [inline: true],
-  #     content: ~s{Enum.map([], fn (_) -> "ok" end)}
-  #   }]
-  #
-  #   expected = ~s{<%= Enum.map([], fn (_) -> "ok" end) %>}
-  #   assert Compiler.compile(tree) == expected
-  # end
-  #
-  # test "renders eex code with multi-line functions" do
-  #   tree = [%EExNode{
-  #     attributes: [inline: true],
-  #     content: ~s{Enum.map [], fn (_) ->},
-  #     children: [%TextNode{content: "test"}]
-  #   }]
-  #
-  #   expected = ~s{<%= Enum.map [], fn (_) -> %>test<% end %>}
-  #   assert Compiler.compile(tree) == expected
-  # end
-  #
+  test "renders eex attributes" do
+    tree = [%HTMLNode{name: "div",
+      attributes: [{"id", {:eex, "variable"}}, {"class", ["class"]}]}
+    ]
+    expected = """
+    <div
+    <% slim__k = "id"; slim__v = variable %>
+    <%= if slim__v do %>
+     <%= slim__k %>
+    <%= unless slim__v == true do %>
+    ="<%= slim__v %>"<% end %><% end %> class="class">
+    </div>
+    """ |> String.replace("\n", "")
+    assert Compiler.compile(tree) == expected
+  end
 
-  #
-  # test "renders boolean attributes" do
-  #   tree = [
-  #     %HTMLNode{
-  #       tag: "input",
-  #       attributes: [class: ["class"],
-  #       required: {:eex, content: "true"}]}
-  #   ]
-  #   assert Compiler.compile(tree) == ~s(<input class="class" required>)
-  #   tree = [
-  #     %HTMLNode{
-  #       tag: "input",
-  #       attributes: [class: ["class"],
-  #       required: {:eex, content: "false"}]}
-  #   ]
-  #   assert Compiler.compile(tree) == ~s(<input class="class">)
-  # end
-  #
-  # test "renders eex" do
-  #   tree = [
-  #     %HTMLNode{tag: :title,
-  #       children: [%EExNode{content: "site_title",
-  #           attributes: [inline: true]}]}
-  #   ]
-  #   expected = "<title><%= site_title %></title>"
-  #   assert Compiler.compile(tree) == expected
-  # end
+  test "renders eex" do
+    tree = [%EExNode{
+      content: ~s(number_input f, :amount, class: "js-donation-amount"),
+      output: true}
+    ]
+    expected = ~s(<%= number_input f, :amount, class: "js-donation-amount" %>)
+    assert Compiler.compile(tree) == expected
+  end
+
+  test "inserts 'end' tokens for do blocks and anonymous functions" do
+    tree = [
+      %EExNode{content: "Enum.map stars, fn star ->", output: true,
+        children: [[%EExNode{content: ~s(star <> "s"), output: true}]]},
+      %EExNode{content: "if welcome do", output: true,
+        children: [[%VerbatimTextNode{content: ["Hello!"]}]]}
+    ]
+    expected = ~S"""
+    <%= Enum.map stars, fn star -> %>
+    <%= star <> "s" %>
+    <% end %>
+    <%= if welcome do %>
+    Hello!
+    <% end %>
+    """ |> String.replace("\n", "")
+    assert Compiler.compile(tree) == expected
+  end
+
+  test "does not insert 'end' tokens for inline blocks" do
+    tree = [
+      %EExNode{content: ~s(if true, do: "ok"), output: true},
+      %EExNode{content: ~s{Enum.map([], fn (_) -> "ok" end)}, output: true}
+    ]
+    expected = ~S"""
+    <%= if true, do: "ok" %>
+    <%= Enum.map([], fn (_) -> "ok" end) %>
+    """ |> String.replace("\n", "")
+    assert Compiler.compile(tree) == expected
+  end
+
+  test "renders boolean attributes" do
+    tree = [
+      %HTMLNode{name: "input",
+        attributes: [{"class", "class"}, {"required", {:eex, "true"}}]},
+      %HTMLNode{name: "input",
+        attributes: [{"class", "class"}, {"required", {:eex, "false"}}]}
+    ]
+    expected = ~S"""
+    <input class="class" required>
+    <input class="class">
+    """ |> String.replace("\n", "")
+    assert Compiler.compile(tree) == expected
+  end
 end
