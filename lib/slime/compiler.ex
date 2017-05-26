b55e5cc0b44caff2e926ddb21755bc239df908dc
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-05-26 23:00:31 +0700

    render -> compile

--- lib/slime/compiler.ex
+++ lib/slime/compiler.ex
@@ -18,12 +18,45 @@ defmodule Slime.Compiler do
   )
 
   def compile([]), do: ""
-  def compile(tree) do
-    tree
-    |> Enum.map(&render(&1))
+  def compile([h | tree]) do
+    [h | tree]
+    |> Enum.map(&compile(&1))
     |> Enum.join
     |> String.replace("\r", "")
   end
+  def compile(%DoctypeNode{name: name}), do: Doctype.for(name)
+  def compile(%VerbatimTextNode{content: content}), do: compile(content)
+  def compile(%HTMLNode{name: name, spaces: spaces} = node) do
+    attrs = Enum.map(node.attributes, &render_attribute/1)
+    tag_head = Enum.join([name | attrs])
+
+    body = cond do
+      node.closed            -> "<" <> tag_head <> "/>"
+      name in @void_elements -> "<" <> tag_head <> ">"
+      :otherwise             -> "<" <> tag_head <> ">"
+        <> compile(node.children) <> "</" <> name <> ">"
+    end
+
+    leading_space(spaces) <> body <> trailing_space(spaces)
+  end
+  def compile(%EExNode{content: code, spaces: spaces, output: output} = node) do
+    opening = (if output, do: "<%= ", else: "<% ") <> code <> " %>"
+    closing = if Regex.match?(~r/(fn.*->| do)\s*$/, code) do
+      "<% end %>"
+    else
+      ""
+    end
+    body = opening <> compile(node.children) <> closing
+
+    leading_space(spaces) <> body <> trailing_space(spaces)
+  end
+  def compile(%InlineHTMLNode{content: content, children: children}) do
+    compile(content) <> compile(children)
+  end
+  def compile(%HTMLCommentNode{content: content}) do
+    "<!--" <> compile(content) <> "-->"
+  end
+  def compile(raw), do: raw
 
   defp render_attribute({_, []}), do: ""
   defp render_attribute({_, ""}), do: ""
@@ -53,41 +86,6 @@ defmodule Slime.Compiler do
       ~s( #{to_string(name)}="#{value}")
     end
   end
-
-  defp render(%DoctypeNode{name: name}), do: Doctype.for(name)
-  defp render(%VerbatimTextNode{content: content}), do: render(content)
-  defp render(%HTMLNode{name: name, spaces: spaces} = node) do
-    attrs = Enum.map(node.attributes, &render_attribute/1)
-    tag_head = Enum.join([name | attrs])
-
-    body = cond do
-      node.closed            -> "<" <> tag_head <> "/>"
-      name in @void_elements -> "<" <> tag_head <> ">"
-      :otherwise             -> "<" <> tag_head <> ">"
-        <> compile(node.children) <> "</" <> name <> ">"
-    end
-
-    leading_space(spaces) <> body <> trailing_space(spaces)
-  end
-  defp render(%EExNode{content: code, spaces: spaces, output: output} = node) do
-    opening = (if output, do: "<%= ", else: "<% ") <> code <> " %>"
-    closing = if Regex.match?(~r/(fn.*->| do)\s*$/, code) do
-      "<% end %>"
-    else
-      ""
-    end
-    body = opening <> compile(node.children) <> closing
-
-    leading_space(spaces) <> body <> trailing_space(spaces)
-  end
-  defp render(%InlineHTMLNode{content: content, children: children}) do
-    render(content) <> compile(children)
-  end
-  defp render(%HTMLCommentNode{content: content}) do
-    "<!--" <> render(content) <> "-->"
-  end
-  defp render([h | t]), do: [h | t] |> Enum.map(&render(&1)) |> Enum.join
-  defp render(raw), do: raw
 
   defp leading_space(%{leading: true}), do: " "
   defp leading_space(_), do: ""
